28f39aa566610bd60cf70337fa3d5936dba0e3a8
hokula <adelhokula18@gmail.com>
2021-03-18 01:18:29 +0100

    Dot Notation

---
+++ ex02/src/index.js
@@ -0,0 +1,16 @@
+var myClothes = {
+  "hat": "Fedora",
+  "shirt": "Nike",
+  "shoes": "Converse",
+};
+function myFunction(myObj) {
+  var hatValue = myClothes.hat;
+  var shirtValue = myClothes.shirt;
+  var shoesValue = myClothes.shoes;
+  return myObj;
+}
+console.log(myFunction(myClothes.hat));
+console.log(myFunction(myClothes.shirt));
+console.log(myFunction(myClothes.shoes));
+
+module.exports = myFunction(myClothes);
